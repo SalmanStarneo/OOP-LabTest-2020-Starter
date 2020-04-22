e6ca3c920af11a5a47dc5e570366420ba2a6f962
SalmanStarneo <salman.alsamiri@gmail.com>
2020-04-22 10:37:10 +0100

    displaying the tasks name with the grid

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -62,29 +62,34 @@ public class Gantt extends PApplet
 			float y = map(i, 0,tasksArray.size(), 100, height-100);
 			noStroke();
             fill(i*tA.getStarts(),255,255);
-            rect(x, y, tA.getStarts()+tA.getEnds(), 50);
+			rect(x, y, tA.getStarts()+tA.getEnds(), 50);
+			fill(255);
+			textAlign(CENTER, CENTER);
+			text(tA.getTasks(), 50, y);
 			
         }
 	}
 
-	    
     public void drawGrid()
     {
-        float border = width * 0.05f;
-
+        float border = width * 0.06f;
+		float x=0;
         stroke(0, 0, 255);
         textAlign(CENTER, CENTER);
 		for(int i = 1 ; i <= 30 ; i ++)
         {
-			// Task tA = tasksArray.get(i);
-            float x = map(i, 1, 30, border, width - border);
+			
+            x = map(i, 1, 30, 100, width - 50);
             line(x, border, x, height - border);
             // line(border, x, width - border, x);    
             
             fill(255);
-            text(i, x, border / 2);
-            // text(i, border / 2, x);
-        }
+			text(i, x, border / 2);
+		}
+		// for(Task tA:tasksArray)
+		// {
+			
+		// }
     }
 	
 	public void draw()
