deed71c2854aab2524326d156dc50626683eeaf3
SalmanStarneo <salman.alsamiri@gmail.com>
2020-04-22 18:59:42 +0100

    wokring on the mouse pressed and grapped still, distance is not easilly to track down

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -15,14 +15,18 @@ public class Gantt extends PApplet
 		size(800, 600);
 		borderX = width * 0.9f;
 		borderY= height *0.06f;
-		mouseKeycode=0;
+		mouseKeycode1=-1;
+		mouseKeycode2=-2;
+		taskSatrtpoint=0;
+		taskEndpoint=0;
 	}
 	//needed global variables
 	float borderY;
 	float borderX;
 	float taskSatrtpoint;
 	float taskEndpoint;
-	float mouseKeycode;
+	float mouseKeycode1;
+	float mouseKeycode2;
 
 	public void loadTasks()
 	{
@@ -45,28 +49,61 @@ public class Gantt extends PApplet
 	
 	public void mousePressed()
 	{
+		for(int m=0 ; m<tasksArray.size() ;m++)
+		{
+			Task tA =tasksArray.get(m);
+			float mouseHeighX1= map(tA.getStarts(), 1, 30, 90, borderX);
+			float mouseHeighX2 = map(tA.getEnds(), 1, 30, 90, borderX);
+			float mouseHeighY=map(m, 0,tasksArray.size(), 100, height-borderY);
+			if (dist(mouseX, mouseY, mouseHeighX1, mouseHeighY)<dist(mouseX, mouseY, mouseHeighX2, mouseHeighY))
+			{
+				if(mouseKeycode1==-1){	
+					println("Mouse pressed L");
+					mouseKeycode1=m;	
+				}
+				else
+				{
+					mouseKeycode1=-1;
+				}
+				
+			}
+			else if(dist(mouseX, mouseY, mouseHeighX2, mouseHeighY)>dist(mouseX, mouseY, mouseHeighX1, mouseHeighY))
+			{
+				if(mouseKeycode2==-1)
+				{
+					
+					mouseKeycode2=m;
+					println("Mouse pressed R");
+				}
+				else
+				{
+				
+					mouseKeycode2=-1;
+				}
+			}
+		}
 		
-		if ((mouseX > 88 && mouseX < borderX && mouseY>borderY && mouseY<height-borderY))
-		{
-			println("Mouse pressed");
-			mouseKeycode=1;	
-			
-		}
-		else
-		{
-			mouseKeycode=0;
-		}
        
 		
 	}
 	
 	public void mouseDragged() 
 	{
+			
+			if (mouseKeycode1!=-1 && mouseKeycode2==-1)
+			{
+				fill(255);
+				ellipse(mouseX, mouseY, 90, 90);
+				// taskSatrtpoint=+20;
+				println("Mouse Dragged L");	
+			}
+			else if (mouseKeycode2!=-1 && mouseKeycode1==-1)
+			{
+				fill(0,255,255);
+				ellipse(mouseX, mouseY, 90, 90);
+				println("Mouse Dragged R");	
+			}
 		
-		while (mouseKeycode==1)
-		{
-			println("Mouse Dragged");	
-		}
 		
 	}
 
@@ -86,8 +123,8 @@ public class Gantt extends PApplet
         for(Task tA:tasksArray)
         {
 
-			xStart = map(tA.getStarts(), 1, 30, 90, borderX);
-			xEnd = map(tA.getEnds(), 1, 30, 90, borderX);
+			xStart = map(tA.getStarts()-taskSatrtpoint, 1, 30, 90, borderX);
+			xEnd = map(tA.getEnds()+taskEndpoint, 1, 30, 90, borderX);
 			float taskwidth= xEnd-xStart;
 			float y = map(i, 0,tasksArray.size(), 100, height-borderY);
 			noStroke();
