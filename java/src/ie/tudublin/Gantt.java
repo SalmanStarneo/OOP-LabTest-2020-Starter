f21aeb699cf6ab898cf95427082dd328e99895e4
SalmanStarneo <salman.alsamiri@gmail.com>
2020-04-22 16:29:40 +0100

    fully done with fixing display tasks correctly

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -13,7 +13,11 @@ public class Gantt extends PApplet
 	public void settings()
 	{
 		size(800, 600);
+		borderX = width * 0.06f;
+		borderY= height *0.15f;
 	}
+	float borderY;
+	float borderX;
 
 	public void loadTasks()
 	{
@@ -36,61 +40,78 @@ public class Gantt extends PApplet
 	
 	public void mousePressed()
 	{
-		println("Mouse pressed");	
-	}
+		
+		// for(int i =0; i< tasksArray.size(); i++) {
+			// float y = map(10, 0, tasksArray.size(), borderX, height - borderX);
 
-	public void mouseDragged()
-	{
-		println("Mouse dragged");
+			if (mouseX >= 100 && mouseX < (width-50) && mouseY>=borderX && mouseY<height-borderX) {
+				println("Mouse pressed");	
+               
+            }
+        // }
+		
 	}
+	int value=0;
+	public void mouseDragged() 
+	{
+		value = value + 5;
+		if (value > 255) {
+			value = 0;
+		}
+		if (mouseX > 100 && mouseX < (width-50) && mouseY>borderX && mouseY<height-borderX) {
+			println("Mouse Dragged");	
+		}
+		
+}
 
 	public void setup() 
 	{
 		loadTasks();
 		printTasks();
 		colorMode(HSB);
-		textSize(14);
+		// textSize(14);
 	}
 
 	public void displayTasks()
 	{
-		
-        for(int i = 0; i < tasksArray.size(); i++)
+		int i=0;
+		float x;
+		float x2;
+        for(Task tA:tasksArray)
         {
-            Task tA = tasksArray.get(i);
 
-			float x = map(i, 0,tasksArray.size(), 100, width-100);
+			x = map(tA.getStarts(), 1, 30, 90, width - 60);
+			x2 = map(tA.getEnds(), 1, 30, 90, width - 60);
+			float taskwidth= x2-x;
 			float y = map(i, 0,tasksArray.size(), 100, height-100);
 			noStroke();
-            fill(i*tA.getEnds(),255,255);
-			rect(x, y, tA.getStarts()+tA.getEnds(), 30);
+            fill(i*(tA.getEnds()),255,255);
+			rect(x, y,taskwidth, 30);
+			// rect(x, y,endwidth, 30);
 			fill(255);
 			textAlign(CENTER, CENTER);
 			text(tA.getTasks(), 50, y+10);
+			i++;
 			
         }
 	}
 
     public void drawGrid()
     {
-        float border = width * 0.06f;
+        
 		float x=0;
         stroke(0, 0, 255);
         textAlign(CENTER, CENTER);
-		for(int i = 1 ; i <= 30 ; i ++)
+		for(int i = 1 ; i < 31 ; i++)
         {
 			
-            x = map(i, 1, 30, 100, width - 50);
-            line(x, border, x, height - border);
-            // line(border, x, width - border, x);    
+            x = map(i, 1, 30, 90, width - 60);
+            line(x, borderX, x, height - borderY);
             
             fill(255);
-			text(i, x, border / 2);
+			text(i, x, borderX / 2);
 		}
-		// for(Task tA:tasksArray)
-		// {
-			
-		// }
+		
     }
 	
 	public void draw()
